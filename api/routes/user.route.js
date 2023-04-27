957a28a072996dab5bf50d92ecb8e0f83b9d1dee
HAITAME <haitame.laframe@etu.uae.ac.ma>
2023-04-27 21:52:40 +0100

    First Commit

---
+++ api/routes/user.route.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const router =express.Router();
+
+router.get("/register",)
+router.get("/login",)
+router.get("/logout",)
+
+
+
+export default router;
---
+++ api/server.js
@@ -0,0 +1,52 @@
+import express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+/*
+import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js"
+import conversationRoute from "./routes/conversation.route.js"
+import messageRoute from "./routes/message.route.js"
+import reviewRoute from "./routes/review.route.js"
+import orderRoute from "./routes/order.route.js"
+*/
+import authRoute from "./routes/auth.route.js"
+
+
+
+const port = 8800;
+
+
+const app = express();
+dotenv.config();
+mongoose.set('strictQuery', true);
+
+const connect = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO)
+        console.log("MongoDB connected");
+    } catch (error){
+        console.log(error);
+    }
+};
+
+
+
+app.listen(port, () => {
+    connect();
+    console.log("backend server is running")
+})
+app.use(express.json());
+
+app.use("/api/auth",authRoute);
+/*
+app.use("/api/users",userRoute);
+app.use("/api/gigs",gigRoute);
+app.use("/api/conversations",conversationRoute);
+app.use("/api/messages",messageRoute);
+app.use("/api/reviews",reviewRoute);
+
+app.use("/api/orders",orderRoute);
+*/
+
+
